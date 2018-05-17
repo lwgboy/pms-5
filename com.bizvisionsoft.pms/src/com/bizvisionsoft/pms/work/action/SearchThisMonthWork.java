a7a7679bd1ee03d67658760aa1a3f649504bb523
yangjun <yj@yaozheng.com.cn>
2018-05-17 14:00:22 +0800

    Signed-off-by: yangjun <yj@yaozheng.com.cn>

--- com.bizvisionsoft.pms/src/com/bizvisionsoft/pms/work/action/SearchThisMonthWork.java
+++ com.bizvisionsoft.pms/src/com/bizvisionsoft/pms/work/action/SearchThisMonthWork.java
@@ -1,12 +1,16 @@
 package com.bizvisionsoft.pms.work.action;
 
+import java.util.Calendar;
+
 import org.eclipse.swt.widgets.Event;
 
 import com.bizvisionsoft.annotations.ui.common.Execute;
 import com.bizvisionsoft.annotations.ui.common.Inject;
 import com.bizvisionsoft.annotations.ui.common.MethodParam;
+import com.bizvisionsoft.bruiengine.assembly.GridPart;
 import com.bizvisionsoft.bruiengine.service.IBruiContext;
 import com.bizvisionsoft.bruiengine.service.IBruiService;
+import com.mongodb.BasicDBObject;
 
 public class SearchThisMonthWork {
 
@@ -16,6 +20,16 @@ public class SearchThisMonthWork {
 	@Execute
 	public void execute(@MethodParam(value = Execute.PARAM_CONTEXT) IBruiContext context,
 			@MethodParam(value = Execute.PARAM_EVENT) Event event) {
+		GridPart gridPart = (GridPart) context.getContent();
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.DAY_OF_MONTH, 1);
+		cal.set(Calendar.HOUR_OF_DAY, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MILLISECOND, 0);
+		cal.add(Calendar.MONTH, 1);
+		cal.add(Calendar.MILLISECOND, -1);
+		gridPart.doQuery(new BasicDBObject("planFinish",new BasicDBObject("$lt",cal.getTime())));
 		
 	}
 
--- com.bizvisionsoft.pms/src/com/bizvisionsoft/pms/work/action/SearchThisWeekWork.java
+++ com.bizvisionsoft.pms/src/com/bizvisionsoft/pms/work/action/SearchThisWeekWork.java
@@ -1,5 +1,7 @@
 package com.bizvisionsoft.pms.work.action;
 
+import java.util.Calendar;
+
 import org.eclipse.swt.widgets.Event;
 
 import com.bizvisionsoft.annotations.ui.common.Execute;
@@ -8,6 +10,7 @@ import com.bizvisionsoft.annotations.ui.common.MethodParam;
 import com.bizvisionsoft.bruiengine.assembly.GridPart;
 import com.bizvisionsoft.bruiengine.service.IBruiContext;
 import com.bizvisionsoft.bruiengine.service.IBruiService;
+import com.mongodb.BasicDBObject;
 
 public class SearchThisWeekWork {
 
@@ -18,8 +21,15 @@ public class SearchThisWeekWork {
 	public void execute(@MethodParam(value = Execute.PARAM_CONTEXT) IBruiContext context,
 			@MethodParam(value = Execute.PARAM_EVENT) Event event) {
 		GridPart gridPart = (GridPart) context.getContent();
-		gridPart.openQueryEditor();
-		System.out.println();
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.HOUR_OF_DAY, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MILLISECOND, 0);
+		int i = 8-cal.get(Calendar.DAY_OF_WEEK);
+		cal.add(Calendar.DAY_OF_MONTH, i);
+		cal.add(Calendar.MILLISECOND, -1);
+		gridPart.doQuery(new BasicDBObject("planFinish",new BasicDBObject("$lt",cal.getTime())));
 	}
 
 }
