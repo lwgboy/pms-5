10f6c635ea952132a43415d39f08eec5a77dbaa9
hua <hua@hua17>
2018-05-19 21:22:53 +0800

    Signed-off-by: hua <hua@hua17>

---
+++ com.bizvisionsoft.pms/src/com/bizvisionsoft/pms/resource/assembly/ResourcePlan.java
@@ -0,0 +1,52 @@
+package com.bizvisionsoft.pms.resource.assembly;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.widgets.Composite;
+
+import com.bizvisionsoft.annotations.ui.common.CreateUI;
+import com.bizvisionsoft.annotations.ui.common.Inject;
+import com.bizvisionsoft.bruiengine.assembly.StickerTitlebar;
+import com.bizvisionsoft.bruiengine.service.BruiAssemblyContext;
+import com.bizvisionsoft.bruiengine.service.IBruiService;
+import com.bizvisionsoft.bruiengine.session.UserSession;
+import com.bizvisionsoft.bruiengine.ui.AssemblyContainer;
+
+public class ResourcePlan {
+
+	@Inject
+	private IBruiService brui;
+
+	@Inject
+	private BruiAssemblyContext context;
+
+	@CreateUI
+	public void createUI(Composite parent) {
+		parent.setLayout(new FormLayout());
+
+		StickerTitlebar bar = new StickerTitlebar(parent, null, null).setText("资源计划")
+				.setActions(context.getAssembly().getActions());
+		FormData fd = new FormData();
+		bar.setLayoutData(fd);
+		fd.left = new FormAttachment(0);
+		fd.top = new FormAttachment(0);
+		fd.right = new FormAttachment(100);
+		fd.height = 48;
+
+		Composite content = UserSession.bruiToolkit().newContentPanel(parent);
+		fd = new FormData();
+		content.setLayoutData(fd);
+		fd.left = new FormAttachment(0, 12);
+		fd.top = new FormAttachment(bar, 12);
+		fd.right = new FormAttachment(100, -12);
+		fd.bottom = new FormAttachment(100, -12);
+		content.setLayout(new FillLayout(SWT.VERTICAL));
+		new AssemblyContainer(content, context).setAssembly(brui.getAssembly("项目甘特图（资源分配）")).setServices(brui).create();
+		new AssemblyContainer(content, context).setAssembly(brui.getAssembly("项目进度计划表")).setServices(brui).create();
+
+	}
+
+}
