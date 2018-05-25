3ce5b216f456559e068c0985b000f645de7edc70
hua <hua@hua17>
2018-05-26 01:12:13 +0800

    Signed-off-by: hua <hua@hua17>

--- com.bizvisionsoft.pms/src/com/bizvisionsoft/pms/project/ProjectBoardRender.java
+++ com.bizvisionsoft.pms/src/com/bizvisionsoft/pms/project/ProjectBoardRender.java
@@ -21,6 +21,7 @@ import com.bizvisionsoft.bruiengine.util.BruiColors.BruiColor;
 import com.bizvisionsoft.service.CommonService;
 import com.bizvisionsoft.service.ProjectService;
 import com.bizvisionsoft.service.WorkService;
+import com.bizvisionsoft.service.model.News;
 import com.bizvisionsoft.service.model.Project;
 import com.bizvisionsoft.service.model.ProjectBoardInfo;
 import com.bizvisionsoft.service.model.ProjectStatus;
@@ -102,15 +103,18 @@ public class ProjectBoardRender {
 		GridItem gridItem = (GridItem) cell.getViewerRow().getItem();
 		gridItem.setHeight(64);
 		gridItem.setBackground(BruiColors.getColor(BruiColor.Grey_50));
+
+		String warrningText = "超期";// TODO
 		StringBuffer sb = new StringBuffer();
 		sb.append(
 				"<div style='float:right;margin-right:16px;margin-top:0px;'><a class='layui-btn layui-btn-primary layui-btn-sm' href='openProject/"
 						+ pj.get_id() + "' target='_rwt'><i class='layui-icon'>&#xe602;</i></a></div>");
 
-		sb.append("<div style='font-size: 22px;'>" + pj.getId() + " / " + pj.getName() + "</div>");
+		sb.append("<div style='font-size: 22px;'>" +pj.getName() + "</div>");
 		sb.append("<div style='width:100%;margin-top:4px;display:inline-flex;justify-content:space-between;'><div>计划: "
 				+ new SimpleDateFormat("yyyy/MM/dd").format(pj.getPlanStart()) + " ~ "
-				+ new SimpleDateFormat("yyyy/MM/dd").format(pj.getPlanFinish()) + "</div>");
+				+ new SimpleDateFormat("yyyy/MM/dd").format(pj.getPlanFinish())
+				+ " <span class='layui-badge layui-bg-red'>" + warrningText + "</span></div>");
 		sb.append("<div style='margin-right:16px;'>项目经理: " + pj.getPmInfo() + "</div></div>");
 
 		cell.setText(sb.toString());
@@ -118,51 +122,49 @@ public class ProjectBoardRender {
 
 	private void renderContent(ViewerCell cell, Project pj) {
 		GridItem gridItem = (GridItem) cell.getViewerRow().getItem();
-		gridItem.setHeight(352);
+		int height = 32;// 顶和底间距
 		gridItem.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_WHITE));
 		StringBuffer sb = new StringBuffer();
+
 		// 如果项目有阶段，显示阶段状态。
 		if (pj.isStageEnable()) {
+			height += 46;
 			sb.append(getProjectStageInfo(pj));
 		}
+
 		// 添加项目时间线
-		sb.append(getProjectNews(pj));
+		List<News> news = Services.get(CommonService.class).getRecentNews(pj.get_id(), 5);
+		if (news.size() > 0) {
+			height += 24;
+			sb.append("<ul class='layui-timeline' style='margin-top:24px;margin-right:32px;'>");
+			for (int i = 0; i < news.size(); i++) {
+				sb.append("<li class='layui-timeline-item' style='padding-bottom:8px;'>");
+				sb.append("<i class='layui-icon layui-timeline-axis'>&#xe63f;</i>");
+				sb.append("<div class='layui-timeline-content layui-text'>");
+				sb.append(
+						"<div class='layui-timeline-title' style='white-space:nowrap;overflow:hidden;text-overflow:ellipsis;width:100%;'>"
+								+ news.get(i).getSummary() + "</div>");
+				sb.append("</div></li>");
+				height += 40;
+			}
+			sb.append("</ul>");
+		}
 
 		// 添加指标
-		sb.append(getProjectIndicators(pj));
-
-		cell.setText(sb.toString());
-	}
-
-	private String getProjectIndicators(Project pj) {
-		StringBuffer text = new StringBuffer();
-		text.append(
-				"<div style='margin-top:0px;width: 100%;display:inline-flex;justify-content:center;padding-right: 36px;'>");
+		height += 46;
 		// TODO
-		text.append(
+		sb.append(
+				"<div style='margin-top:0px;width: 100%;display:inline-flex;justify-content:center;padding-right: 36px;'>");
+		sb.append(
 				"<div class='brui_indicator normal' style='font-size:16px;font-weight:lighter;flex:auto;flex-basis:33%;margin:0px 8px;'>63.0%<br/>进度完成率</div>");
-		text.append(
+		sb.append(
 				"<div class='brui_indicator info' style='font-size:16px;font-weight:lighter;flex:auto;flex-basis:33%;margin:0px 8px;'>32.0%<br/>工作量完成率</div>");
-		text.append(
+		sb.append(
 				"<div class='brui_indicator warning' style='font-size:16px;font-weight:lighter;flex:auto;flex-basis:33%;margin:0px 8px;'>52.0%<br/>工期完成率</div>");
-		text.append("</div>");
-		return text.toString();
-	}
+		sb.append("</div>");
 
-	private String getProjectNews(Project pj) {
-		StringBuffer text = new StringBuffer();
-		text.append("<ul class='layui-timeline' style='margin-top:24px;margin-right:32px;'>");
-		Services.get(CommonService.class).getRecentNews(pj.get_id(), 5).forEach(n -> {
-			text.append("<li class='layui-timeline-item' style='padding-bottom:8px;'>");
-			text.append("<i class='layui-icon layui-timeline-axis'>&#xe63f;</i>");
-			text.append("<div class='layui-timeline-content layui-text'>");
-			text.append(
-					"<div class='layui-timeline-title' style='white-space:nowrap;overflow:hidden;text-overflow:ellipsis;width:100%;'>"
-							+ n.getSummary() + "</div>");
-			text.append("</div></li>");
-		});
-		text.append("</ul>");
-		return text.toString();
+		gridItem.setHeight(height);
+		cell.setText(sb.toString());
 	}
 
 	private String getProjectStageInfo(Project pj) {
