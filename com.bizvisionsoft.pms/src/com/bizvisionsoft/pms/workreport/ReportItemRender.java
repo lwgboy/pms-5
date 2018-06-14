bf623bf32c67e7d6b72c9c2089490e57099030dd
hua <hua@www.techsmith.com>
2018-06-14 10:41:00 +0800

    Signed-off-by: hua <hua@www.techsmith.com>

---
+++ com.bizvisionsoft.pms/src/com/bizvisionsoft/pms/workreport/ReportItemRender.java
@@ -0,0 +1,106 @@
+package com.bizvisionsoft.pms.workreport;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Optional;
+
+import org.eclipse.jface.viewers.ViewerCell;
+import org.eclipse.nebula.jface.gridviewer.GridTreeViewer;
+import org.eclipse.nebula.widgets.grid.GridColumn;
+import org.eclipse.swt.SWT;
+
+import com.bizvisionsoft.annotations.ui.common.Inject;
+import com.bizvisionsoft.annotations.ui.common.MethodParam;
+import com.bizvisionsoft.annotations.ui.grid.GridRenderColumnHeader;
+import com.bizvisionsoft.annotations.ui.grid.GridRenderUICreated;
+import com.bizvisionsoft.annotations.ui.grid.GridRenderUpdateCell;
+import com.bizvisionsoft.bruicommons.model.Column;
+import com.bizvisionsoft.bruiengine.assembly.GridPartDefaultRender;
+import com.bizvisionsoft.bruiengine.service.BruiAssemblyContext;
+import com.bizvisionsoft.bruiengine.service.IBruiService;
+import com.bizvisionsoft.service.model.Work;
+import com.bizvisionsoft.service.model.WorkReportItem;
+
+public class ReportItemRender extends GridPartDefaultRender {
+
+	@Inject
+	private BruiAssemblyContext context;
+
+	@Inject
+	private IBruiService brui;
+
+	private GridTreeViewer viewer;
+
+	@GridRenderUICreated
+	private void uiCreated() {
+		viewer = (GridTreeViewer) context.getContent("viewer");
+		viewer.getGrid().addListener(SWT.Selection, e -> {
+			if (e.text != null) {
+				if (e.text.startsWith("startWork/")) {
+				} else if (e.text.startsWith("finishWork/")) {
+				} else if (e.text.split("/").length > 1) {
+				}
+			}
+		});
+	}
+
+	@Override
+	@GridRenderUpdateCell
+	public void renderCell(@MethodParam(GridRenderUpdateCell.PARAM_CELL) ViewerCell cell,
+			@MethodParam(GridRenderUpdateCell.PARAM_COLUMN) Column column,
+			@MethodParam(GridRenderUpdateCell.PARAM_VALUE) Object value,
+			@MethodParam(GridRenderUpdateCell.PARAM_IMAGE) Object image) {
+		WorkReportItem reportItem = (WorkReportItem) cell.getElement();
+		String text = "";
+		if ("reportWorkPlan".equals(column.getName())) {
+			text = getWorkPlanText(reportItem);
+		} else if ("reportWorkStatement".equals(column.getName())) {
+			text = getWorkStatementText(reportItem);
+		} else if ("reportProblems".equals(column.getName())) {
+			text = getWorkProblemsText(reportItem);
+		} else if ("reportPMRemark".equals(column.getName())) {
+			text = getPMRemarkText(reportItem);
+		}
+		cell.setText(text);
+	}
+
+	private String getPMRemarkText(WorkReportItem ri) {
+		return "getPMRemarkText";
+	}
+
+	private String getWorkProblemsText(WorkReportItem ri) {
+		return "getWorkProblemsText";
+	}
+
+	private String getWorkStatementText(WorkReportItem ri) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("<div>" + Optional.ofNullable(ri.getStatement()).orElse("") + "</div>");
+		if (!ri.isConfirmed() && brui.getCurrentUserId().equals(ri.getReportUserId())) {
+			sb.append("<button class='layui-btn layui-btn-sm layui-btn-primary'>"
+					+ "<i class='layui-icon  layui-icon-edit'></i>" + "</button>");
+		}
+		return sb.toString();
+	}
+
+	private String getWorkPlanText(WorkReportItem ri) {
+		Work work = ri.getWork();
+		StringBuffer sb = new StringBuffer();
+		sb.append("<div class='label_title'>" + work.getFullName() + "</div>");
+		sb.append("<div>计划: " + formatText(work.getPlanStart()) + " ~ " + formatText(work.getPlanFinish()) + "</div>");
+		sb.append("<div>实际: " + formatText(work.getActualStart()) + " ~ " + formatText(work.getActualFinish())
+				+ "</div>");
+		return sb.toString();
+	}
+
+	@Override
+	@GridRenderColumnHeader
+	public void renderColumnHeader(@MethodParam(GridRenderColumnHeader.PARAM_COLUMN_WIDGET) GridColumn col,
+			@MethodParam(GridRenderColumnHeader.PARAM_COLUMN) Column column) {
+		super.renderColumnHeader(col, column);
+	}
+
+	private String formatText(Date date) {
+		return Optional.ofNullable(date).map(d -> new SimpleDateFormat("yyyy-MM-dd").format(d)).orElse("");
+	}
+
+}
