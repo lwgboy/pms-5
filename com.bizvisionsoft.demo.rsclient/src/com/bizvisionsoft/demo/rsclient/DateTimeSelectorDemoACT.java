79f16895c9c0178a24d61e6ef2a4b0b71d4dcc07
hua <hua@hua17>
2018-06-19 22:10:34 +0800

    Signed-off-by: hua <hua@hua17>

--- com.bizvisionsoft.demo.rsclient/src/com/bizvisionsoft/demo/rsclient/DateTimeSelectorDemoACT.java
+++ com.bizvisionsoft.demo.rsclient/src/com/bizvisionsoft/demo/rsclient/DateTimeSelectorDemoACT.java
@@ -1,10 +1,7 @@
 package com.bizvisionsoft.demo.rsclient;
 
-import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Event;
 
-import com.bizivisionsoft.widgets.datetime.DateTimeSelector;
-import com.bizivisionsoft.widgets.datetime.DateTimeSetting;
 import com.bizvisionsoft.annotations.ui.common.Execute;
 import com.bizvisionsoft.annotations.ui.common.Inject;
 import com.bizvisionsoft.annotations.ui.common.MethodParam;
@@ -17,13 +14,6 @@ public class DateTimeSelectorDemoACT {
 
 	@Execute
 	public void execute(@MethodParam(Execute.PARAM_EVENT) Event event) {
-		//演示直接选择日期
-		final DateTimeSelector dts = new DateTimeSelector(event.item);
-		dts.addListener(SWT.Selection, l->{
-			System.out.println(dts.getDate());
-			System.out.println(dts.getEndDate());
-		});
-		dts.show(DateTimeSetting.date());
 	}
 
 }
