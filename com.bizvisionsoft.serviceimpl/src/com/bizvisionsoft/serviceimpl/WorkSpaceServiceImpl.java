241a66b8db25d34bc65b26ba0738e7f5e275d353
hua <hua@hua17>
2018-07-13 11:41:19 +0800

    Signed-off-by: hua <hua@hua17>

--- com.bizvisionsoft.serviceimpl/src/com/bizvisionsoft/serviceimpl/WorkSpaceServiceImpl.java
+++ com.bizvisionsoft.serviceimpl/src/com/bizvisionsoft/serviceimpl/WorkSpaceServiceImpl.java
@@ -197,20 +197,20 @@ public class WorkSpaceServiceImpl extends BasicServiceImpl implements WorkSpaceS
 			}
 		} else {
 			Document doc = c("workspace")
-					.aggregate(Arrays.asList(new Document("$match", new Document("_id", workspace.getSpace_id())),
+					.aggregate(Arrays.asList(new Document("$match", new Document("space_id", workspace.getSpace_id())),
 							new Document("$group",
 									new Document("_id", null).append("finish", new Document("$max", "$planFinish")))))
 					.first();
 			if (workspace.getWork_id() != null) {
 				Work work = new WorkServiceImpl().getWork(workspace.getWork_id());
-				if (work.getPlanFinish().after(doc.getDate("finish"))) {
+				if (work.getPlanFinish().before(doc.getDate("finish"))) {
 					Result result = Result.checkoutError("完成时间超过阶段限定。", Result.CODE_UPDATEMANAGEITEM);
 					result.data = new BasicDBObject("name", work.getText());
 					return result;
 				}
 			} else {
 				Project project = new ProjectServiceImpl().get(workspace.getProject_id());
-				if (project.getPlanFinish().after(doc.getDate("finish"))) {
+				if (project.getPlanFinish().before(doc.getDate("finish"))) {
 					Result result = Result.checkoutError("完成时间超过项目限定。", Result.CODE_UPDATEMANAGEITEM);
 					result.data = new BasicDBObject("name", project.getName());
 					return result;
