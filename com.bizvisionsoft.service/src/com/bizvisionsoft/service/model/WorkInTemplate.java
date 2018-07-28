aa9266474f0248b5457f3532183c8a39b4fd9b45
gdiyang <gdiyang@lode>
2018-07-28 10:16:23 +0800

    Signed-off-by: gdiyang <gdiyang@lode>

--- com.bizvisionsoft.service/src/com/bizvisionsoft/service/model/WorkInTemplate.java
+++ com.bizvisionsoft.service/src/com/bizvisionsoft/service/model/WorkInTemplate.java
@@ -460,4 +460,29 @@ public class WorkInTemplate implements IWorkPackageMaster {
 		return this;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((_id == null) ? 0 : _id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		WorkInTemplate other = (WorkInTemplate) obj;
+		if (_id == null) {
+			if (other._id != null)
+				return false;
+		} else if (!_id.equals(other._id))
+			return false;
+		return true;
+	}
+
 }
--- com.bizvisionsoft.service/src/com/bizvisionsoft/service/model/WorkLinkInTemplate.java
+++ com.bizvisionsoft.service/src/com/bizvisionsoft/service/model/WorkLinkInTemplate.java
@@ -160,4 +160,29 @@ public class WorkLinkInTemplate {
 		return this;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((_id == null) ? 0 : _id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		WorkLinkInTemplate other = (WorkLinkInTemplate) obj;
+		if (_id == null) {
+			if (other._id != null)
+				return false;
+		} else if (!_id.equals(other._id))
+			return false;
+		return true;
+	}
+
 }
