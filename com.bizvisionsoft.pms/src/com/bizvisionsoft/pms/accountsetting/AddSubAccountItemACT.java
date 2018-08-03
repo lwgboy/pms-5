e625a60fb8f88c276c0655302793439771978768
hua <hua@www.techsmith.com>
2018-08-03 22:59:02 +0800

    简化更名

--- com.bizvisionsoft.pms/src/com/bizvisionsoft/pms/accountsetting/AddSubAccountItemACT.java
+++ com.bizvisionsoft.pms/src/com/bizvisionsoft/pms/accountsetting/AddSubAccountItemACT.java
@@ -1,4 +1,4 @@
-package com.bizvisionsoft.pms.accountsetting.action;
+package com.bizvisionsoft.pms.accountsetting;
 
 import com.bizvisionsoft.annotations.ui.common.Execute;
 import com.bizvisionsoft.annotations.ui.common.Inject;
@@ -11,7 +11,7 @@ import com.bizvisionsoft.service.CommonService;
 import com.bizvisionsoft.service.model.AccountItem;
 import com.bizvisionsoft.serviceconsumer.Services;
 
-public class AddSubAccountItem {
+public class AddSubAccountItemACT {
 
 	@Inject
 	private IBruiService bruiService;
