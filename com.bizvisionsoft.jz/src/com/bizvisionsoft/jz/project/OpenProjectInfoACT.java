b40f0c0fafcdc9011e5d0d379d86767ad71a6021
gdiyang <gdiyang@lode>
2018-08-24 17:04:46 +0800

    1依据不同的项目类型显示不同的项目编辑界面。 2主页-项目管理中增加部门项目和外协项目独立的显示table 2.1部门项目table中只显示当前用户所在部门的项目 2.2外协项目table显示全部的外协。 2.3外协项目table只有具有权限的用户（外协项目管理员）才能查看
    Signed-off-by: gdiyang <gdiyang@lode>

---
+++ com.bizvisionsoft.jz/src/com/bizvisionsoft/jz/project/OpenProjectInfoACT.java
@@ -0,0 +1,58 @@
+package com.bizvisionsoft.jz.project;
+
+import java.util.Optional;
+
+import org.bson.types.ObjectId;
+import org.eclipse.swt.widgets.Event;
+
+import com.bizivisionsoft.widgets.util.Layer;
+import com.bizvisionsoft.annotations.AUtil;
+import com.bizvisionsoft.annotations.ui.common.Execute;
+import com.bizvisionsoft.annotations.ui.common.Inject;
+import com.bizvisionsoft.annotations.ui.common.MethodParam;
+import com.bizvisionsoft.bruiengine.service.IBruiContext;
+import com.bizvisionsoft.bruiengine.service.IBruiService;
+import com.bizvisionsoft.bruiengine.ui.Editor;
+import com.bizvisionsoft.service.ProjectService;
+import com.bizvisionsoft.service.datatools.FilterAndUpdate;
+import com.bizvisionsoft.service.model.Project;
+import com.bizvisionsoft.serviceconsumer.Services;
+import com.mongodb.BasicDBObject;
+
+public class OpenProjectInfoACT {
+
+	@Inject
+	private IBruiService bruiService;
+
+	@Execute
+	public void execute(@MethodParam(Execute.PARAM_CONTEXT) IBruiContext context,
+			@MethodParam(Execute.PARAM_EVENT) Event event) {
+
+		ObjectId project_id = context.getRootInput(Project.class, false).get_id();
+		Project project = Services.get(ProjectService.class).get(project_id);
+
+		String title = Optional.ofNullable(AUtil.readTypeAndLabel(project)).orElse("");
+
+		String assemblyName = "项目编辑器";
+		if (Project.PROJECTTYPE_DEPT.equals(project.getProjectType()))
+			assemblyName = "部门项目编辑器";
+		else if (Project.PROJECTTYPE_EXTERNAL.equals(project.getProjectType()))
+			assemblyName = "外协项目编辑器";
+
+		new Editor<Project>(bruiService.getAssembly(assemblyName), context).setInput(project).setTitle(title)
+				.setEditable(false).ok((r, proj) -> {
+					try {
+						Services.get(ProjectService.class).update(
+								new FilterAndUpdate().filter(new BasicDBObject("_id", project.get_id())).set(r).bson());
+						AUtil.simpleCopy(proj, project);
+					} catch (Exception e) {
+						String message = e.getMessage();
+						if (message.indexOf("index") >= 0) {
+							Layer.message("请勿录入相同的项目编号", Layer.ICON_CANCEL);
+						}
+					}
+				});
+
+	}
+
+}
