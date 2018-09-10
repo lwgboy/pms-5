b99e14073daeff3a5f80ea9dd14cbcde02ccdd4a
hua <hua@hua17>
2018-09-10 21:40:02 +0800

    Signed-off-by: hua <hua@hua17>

--- com.bizvisionsoft.pms/src/com/bizvisionsoft/pms/project/action/CloseProject.java
+++ com.bizvisionsoft.pms/src/com/bizvisionsoft/pms/project/action/CloseProject.java
@@ -31,8 +31,8 @@ public class CloseProject {
 		CommandHandler.run(ICommand.Close_Project, //
 				"请确认关闭项目" + project + "。<br/>项目关闭后将禁止所有的项目有关操作，包括项目财务结算，创建或更改项目文档。工作包历史跟踪记录将被清除。", //
 				"项目关闭完成", "项目关闭失败", //
-				() -> service.finishProject(brui.command(id, new Date(), ICommand.Close_Project)), //
-				() -> service.finishProject(brui.command(id, new Date(), ICommand.Close_Project_Ignore_Warrning)), //
+				() -> service.closeProject(brui.command(id, new Date(), ICommand.Close_Project)), //
+				() -> service.closeProject(brui.command(id, new Date(), ICommand.Close_Project_Ignore_Warrning)), //
 				code -> brui.switchPage("项目首页（关闭）", id.toHexString()));
 
 	}
