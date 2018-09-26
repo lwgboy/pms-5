2ca91e0b6f220bd4c9025cf7a6c136e9f1003464
hua <ghuazh@gmail.com>
2018-09-26 15:49:47 +0800

    Signed-off-by: hua <ghuazh@gmail.com>

--- com.bizvisionsoft.pms/src/com/bizvisionsoft/pms/work/gantt/action/EditGantt.java
+++ com.bizvisionsoft.pms/src/com/bizvisionsoft/pms/work/gantt/action/EditGantt.java
@@ -37,8 +37,8 @@ public class EditGantt {
 						brui.switchContent("项目甘特图（编辑）", workspace);
 					} else if (Result.CODE_HASCHECKOUTSUB == result.code) {
 						if (MessageDialog.openConfirm(brui.getCurrentShell(), "提示",
-								"本计划中的  <b style='color: red;'>" +  result.data.get("name")
-										+ "</b>  工作正在由   <b style='color: red;'>" +  result.data.get("username")
+								"本计划中的  <b style='color: red;'>" + result.data.get("name")
+										+ "</b>  工作正在由   <b style='color: red;'>" + result.data.get("username")
 										+ "</b>  进行计划编辑。" + "继续需编辑本计划，将撤销该用户未提交的计划。")) {
 							result = Services.get(WorkSpaceService.class).checkout(workspace, brui.getCurrentUserId(),
 									true);
@@ -48,7 +48,7 @@ public class EditGantt {
 						}
 					}
 				} else {
-					MessageDialog.openError(brui.getCurrentShell(), "提示", "您没有编辑本计划的权限。");
+					MessageDialog.openError(brui.getCurrentShell(), "提示", "计划已经由其他用户检出：" + checkoutUserId);
 				}
 			}
 		}
