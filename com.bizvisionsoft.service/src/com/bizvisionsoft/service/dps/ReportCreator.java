0697a2593b4a667806221fddd1c10ec06d99f52f
hua <ghuazh@gmail.com>
2018-09-30 12:13:27 +0800

    Signed-off-by: hua <ghuazh@gmail.com>

--- com.bizvisionsoft.service/src/com/bizvisionsoft/service/dps/ReportCreator.java
+++ com.bizvisionsoft.service/src/com/bizvisionsoft/service/dps/ReportCreator.java
@@ -2,10 +2,30 @@ package com.bizvisionsoft.service.dps;
 
 import java.io.File;
 import java.io.OutputStream;
+import java.util.HashMap;
 
 public interface ReportCreator {
+	
+	public static final String OUTPUT_HTML = "html";
 
-	void createReport(String dataURL, File templateFile, OutputStream os) throws Exception;
+	public static final String OUTPUT_PDF = "pdf";
 
+	public static final String OUTPUT_DOCX = "docx";
+
+	public static final String OUTPUT_EXCEL = "excel";
+
+	public static final String OUTPUT_PPTX = "pptx";
+
+
+	public default void createReportFromXML(String dataURL, File templateFile, OutputStream os) throws Exception{
+		HashMap<String, String> parameter = new HashMap<String, String>();
+		if (dataURL != null) {
+			parameter.put("FILELIST", dataURL);
+		}
+		createReport(parameter, OUTPUT_PDF, templateFile, os);
+	}
+
+	public void createReport(HashMap<String, String> parameter, String outputType, File templateFile, OutputStream os)
+			throws Exception;
 
 }
