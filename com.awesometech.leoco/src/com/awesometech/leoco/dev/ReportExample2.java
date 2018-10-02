ddf33a807285993166c8272264915f835359dc5b
hua <ghuazh@gmail.com>
2018-10-02 12:44:17 +0800

    Signed-off-by: hua <ghuazh@gmail.com>

--- com.awesometech.leoco/src/com/awesometech/leoco/dev/ReportExample2.java
+++ com.awesometech.leoco/src/com/awesometech/leoco/dev/ReportExample2.java
@@ -10,12 +10,18 @@ public class ReportExample2 {
 
 	@Execute
 	public void execute() {
+		//报表参数为json格式字符串
 		String rptParam = new Document("pipeline", "[{'$match':{'project_id':{'$oid':'5b988ee1a183c92e0c105ede'}}},"
 				+ " {'$lookup' : {'from' : 'project', 'localField' : 'project_id','foreignField' : '_id', 'as' : 'project'}}, "
 				+ "{ '$unwind' : {'path' : '$project'}}]").toJson();
-		JsonObject postParam = new JsonObject().set("rptParam", rptParam).set("template", "simple_parameter2.rptdesign")
-				.set("outputType", "pdf").set("fileName", "example2.pdf");//
-		UserSession.bruiToolkit().downloadServerFile("report", postParam);
+		
+		JsonObject postParam = new JsonObject()//
+				.set("rptParam", rptParam)//指定到报表参数
+				.set("template", "simple_parameter2.rptdesign")//模板文件名
+				.set("outputType", "pdf")//生成PDF文件
+				.set("fileName", "example2.pdf");//下载用的文件名
+		
+		UserSession.bruiToolkit().downloadServerFile("report", postParam);//下载
 	}
 
 }
