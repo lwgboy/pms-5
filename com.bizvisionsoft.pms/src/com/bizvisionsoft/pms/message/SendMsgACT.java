c9c8dd9560081ff99c98329c8c2881d59f2c27e5
hua <ghuazh@gmail.com>
2018-10-10 17:18:45 +0800

    使用浏览器通知替代MessageDialog.openWarning

--- com.bizvisionsoft.pms/src/com/bizvisionsoft/pms/message/SendMsgACT.java
+++ com.bizvisionsoft.pms/src/com/bizvisionsoft/pms/message/SendMsgACT.java
@@ -4,8 +4,8 @@ import com.bizivisionsoft.widgets.util.Layer;
 import com.bizvisionsoft.annotations.ui.common.Execute;
 import com.bizvisionsoft.annotations.ui.common.Inject;
 import com.bizvisionsoft.annotations.ui.common.MethodParam;
-import com.bizvisionsoft.bruiengine.service.BruiService;
 import com.bizvisionsoft.bruiengine.service.IBruiContext;
+import com.bizvisionsoft.bruiengine.service.IBruiService;
 import com.bizvisionsoft.bruiengine.ui.Editor;
 import com.bizvisionsoft.service.CommonService;
 import com.bizvisionsoft.service.model.NewMessage;
@@ -14,12 +14,13 @@ import com.bizvisionsoft.serviceconsumer.Services;
 public class SendMsgACT {
 
 	@Inject
-	private BruiService br;
+	private IBruiService br;
 
 	@Execute
 	public void execute(@MethodParam(Execute.PARAM_CONTEXT) IBruiContext context) {
 		Editor.create("新消息", context, new NewMessage(), false).ok((r, o) -> {
 			o.sender = br.getCurrentConsignerInfo();
+			br.sendMessage(o.sender,o.receiver,o.subject,o.content);
 			Services.get(CommonService.class).sendMessage(o);
 			Layer.message("消息已发送");
 		});
