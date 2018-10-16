f3382d02c1942df77346e0088b90694441eafa32
gdiyang <gdiyang@lode>
2018-10-16 12:05:18 +0800

    增加.site文件修改说明
    Signed-off-by: gdiyang <gdiyang@lode>

--- com.bizvisionsoft.serviceimpl/src/com/bizvisionsoft/serviceimpl/update/PMS0501_pmo.java
+++ com.bizvisionsoft.serviceimpl/src/com/bizvisionsoft/serviceimpl/update/PMS0501_pmo.java
@@ -84,7 +84,7 @@ public class PMS0501_pmo implements Runnable {
 				Service.col(OBSItem.class).insertMany(insertOBSItem);
 				logger.info("完成项目添加PMO团队。");
 			} catch (Exception e) {
-				logger.info("为项目添加PMO团队时出现错误:"+e.getMessage());
+				logger.info("为项目添加PMO团队时出现错误:" + e.getMessage());
 			}
 	}
 
@@ -109,3 +109,103 @@ public class PMS0501_pmo implements Runnable {
 	}
 
 }
+/**
+ * 配置需更新内容：
+ * <p>
+ * 1.组件库-业务管理-增加组织模板的一组组件
+ * <p>
+ * 1.1组织模板组件: 组件名称:组织模板; 组件类型:表格组件;
+ * 取数服务名称:com.bizvisionsoft.service.ProjectTemplateService
+ * 启用顶部标题栏和工具栏;组件标题:组织模板管理
+ * 表格列:id(列名称:id,文本:编号,左对齐,宽:160);name(列名称:name,文本:名称,左对齐,宽:300);description(列名称:description,文本:说明,左对齐,宽:320)
+ * 行操作:删除(操作类型:删除选中对象),编辑(操作类型:编辑或打开选中对象.编辑器组件:组织模板编辑器),打开(操作类型:自定义操作,图标:/img/right.svg,样式:默认,插件标识:com.bizvisionsoft.pms,类名:com.bizvisionsoft.pms.projecttemplate.OpenProjectTemplateACT)
+ * 工具栏操作:创建组织模板:(操作类型:创建新对象,编辑器组件:组织模板编辑器,插件标识:com.bizvisionsoft.service,类名:com.bizvisionsoft.service.model.OBSModule)
+ * <p>
+ * 1.2组织模板编辑器组件:组件名称:组织模板编辑器;标题:组织模板;使用窄模式
+ * 字段:id(名称:id,文本:编号,不可为空,字段类型:单行文本),name(名称:name,文本:名称,不可为空,字段类型:单行文本),description(名称:description,文本:说明,字段类型:单行文本),epsInfos(名称:epsInfos,文本:适用范围,不可为空,字段类型:多个对象选择框,选择器组件:EPS管理)
+ * <p>
+ * 1.3组织模板选择器列表组件: 组件名称:组织模板选择器列表; 组件类型:表格组件;描述:用于项目选择组织模板;
+ * 取数服务名称:com.bizvisionsoft.service.ProjectTemplateService
+ * 表格列:id(列名称:id,文本:编号,左对齐,宽:160);name(列名称:name,文本:名称,左对齐,宽:300);description(列名称:description,文本:说明,左对齐,宽:320)
+ * <p>
+ * 1.4组织模板选择器组件:组件名称:组织模板选择器; 组件类型:弹出式选择器组件;描述:用于项目选择组织模板;表格组件:组织模板选择器列表;
+ * <p>
+ * 1.5OBS模板组织结构图组件: 组件名称:OBS模板组织结构图; 组件类型:树组件;
+ * 取数服务名称:com.bizvisionsoft.service.ProjectTemplateService
+ * 启用顶部标题栏和工具栏;组件标题:组织结构模板 节点操作:
+ * 添加角色:类型:自定义操作,名称:添加角色,文本:添加角色,强制使用文本,图标:/img/add_16_w.svg,风格:一般,对象行为控制,插件标识:com.bizvisionsoft.pms,类名:com.bizvisionsoft.pms.projecttemplate.CreateOBSRoleItemACT;
+ * 创建团队:类型:自定义操作,名称:创建团队,文本:创建团队,强制使用文本,图标:/img/team_w.svg,风格:一般,对象行为控制,插件标识:com.bizvisionsoft.pms,类名:com.bizvisionsoft.pms.projecttemplate.CreateOBSTeamItemACT;;
+ * 指定担任者:类型:自定义操作,名称:指定担任者,文本:指定担任者,强制使用文本,图标:/img/appointment_w.svg,风格:一般,对象行为控制,插件标识:com.bizvisionsoft.pms,类名:com.bizvisionsoft.pms.projecttemplate.AppointmentOBSInTemplateACT;
+ * 编辑:类型:自定义操作,名称:编辑,文本:编辑,强制使用文本,图标:/img/edit_w.svg,风格:一般,对象行为控制,插件标识:com.bizvisionsoft.pms,类名:com.bizvisionsoft.pms.projecttemplate.EditOBSItemACT;
+ * 删除:类型:删除选中对象,名称:删除,文本:删除,强制使用文本,图标:/img/minus_w.svg,风格:警告;
+ * 成员:类型:打开内容区,名称:成员,文本:成员,强制使用文本,图标:/img/people_list_w.svg,风格:信息,内容区组件:组织模板团队成员,原有内容区不关闭;
+ * <p>
+ * 1.6组织模板团队成员组件: 组件名称:组织模板团队成员; 组件类型:表格组件;描述:用于组织模板中编辑团队成员;
+ * 取数服务名称:com.bizvisionsoft.service.OBSService 启用顶部标题栏和工具栏;组件标题:团队成员,显示传入对象名称,
+ * 表格列,行操作同"团队成员(id:162dcc76808)"组件.
+ * 工具栏操作:添加用户:类型:自定义操作,名称:添加用户,图标:/img/add_16_w.svg,风格:一般,插件标识:com.bizvisionsoft.pms,类名:com.bizvisionsoft.pms.projecttemplate.AddOBSInTemplateMember
+ * <p>
+ * 2.组件库-增加报告管理的一组组件
+ * <p>
+ * 2.1日报管理组件: 组件名称:日报管理; 组件类型:表格组件;
+ * 取数服务名称:com.bizvisionsoft.service.WorkReportService
+ * 启用顶部标题栏和工具栏;组件标题:日报管理
+ * 表格列:同"日报(id:163f1e35a8f)"组件
+ * 行操作:打开日报(操作类型:自定义操作,图标:/img/right.svg,样式:默认,插件标识:com.bizvisionsoft.pms,类名:com.bizvisionsoft.pms.workreport.OpenWorkReportACT)
+ * <p>
+ * 2.2周报管理组件: 组件名称:周报管理; 组件类型:表格组件;
+ * 取数服务名称:com.bizvisionsoft.service.WorkReportService
+ * 启用顶部标题栏和工具栏;组件标题:周报管理
+ * 表格列:同"周报(id:163fe7e3c53)"组件
+ * 行操作:打开周报(操作类型:自定义操作,图标:/img/right.svg,样式:默认,插件标识:com.bizvisionsoft.pms,类名:com.bizvisionsoft.pms.workreport.OpenWorkReportACT)
+ * <p>
+ * 2.3月报管理组件: 组件名称:月报管理; 组件类型:表格组件;
+ * 取数服务名称:com.bizvisionsoft.service.WorkReportService
+ * 启用顶部标题栏和工具栏;组件标题:月报管理
+ * 表格列:同"月报(id:163fe9eba7f)"组件
+ * 行操作:打开月报(操作类型:自定义操作,图标:/img/right.svg,样式:默认,插件标识:com.bizvisionsoft.pms,类名:com.bizvisionsoft.pms.workreport.OpenWorkReportACT)
+ * <p>
+ * 2.4日报(id:163f1e35a8f),周报(id:163fe7e3c53),月报(id:163fe9eba7f),待确认的报告(id:1646acf088f),项目月报(id:16401bc7721),项目周报(id:16401bbfa5c),项目日报(id:16401bb95a9)
+ * 行操作"打开月报"修改:(操作类型:自定义操作,图标:/img/right.svg,样式:默认,插件标识:com.bizvisionsoft.pms,类名:com.bizvisionsoft.pms.workreport.OpenWorkReportACT)
+ * <p>
+ * 3.组件库-业务管理-组织管理：组织编辑器（id：162b75519b0）和根组织编辑器（id：162b7bb8236）中项目承担组织字段名改为qualifiedContractor
+ * <p>
+ * 4.开荒环境侧边栏,操作栏目新增子栏目:名称为"更新项目管理组",插件标识:com.bizvisionsoft.onlinedesigner,类名:com.bizvisionsoft.onlinedesigner.systemupdate.SystemUpdateV0501_pmo
+ * <p>
+ * 5.主頁侧边栏修改:
+ * <p>
+ * 5.1新增侧边栏"报告管理",放在"我的报告"栏目下方,角色设置为:项目管理员#项目总监.报告管理栏目下存在3个子栏目:1.日报管理:操作类型为打开内容区,内容区设置为"日报管理";2.周报管理:操作类型为打开内容区,内容区设置为"周报管理";3.月报管理:操作类型为打开内容区,内容区设置为"月报管理"
+ * <p>
+ * 5.2修改成本管理角色设置为:财务经理#财务管理#项目总监
+ * <p>
+ * 5.3修改采购管理角色设置为:供应链经理#供应链管理#项目总监
+ * <p>
+ * 5.4修改生产管理角色设置为:制造经理#制造管理#项目总监
+ * <p>
+ * 6.业务管理侧边栏增加组织,
+ * <p>
+ * 6.1放在"WBS模块"栏目下方.操作类型为打开内容区,内容区设置为"组织模板";
+ * <p>
+ * 6.2组件库-主页-项目主页-项目快速启动(创建状态)面板操作中增加action:套用组织模板,放在"使用项目模板"action下方.套用组织模板操作类型:自定义操作;图标为:/img/org_c.svg;插件标识com.bizvisionsoft.pms;类名:com.bizvisionsoft.pms.project.action.UseOBSModule;角色:PM#PPM
+ * <p>
+ * 6.3组件库-业务管理-项目模板管理-OBS模板-项目模板组织结构图.增加工具栏操作:另存为组织模板.操作类型:自定义操作;强制使用文本;按钮风格:一般;插件标识com.bizvisionsoft.pms;类名:com.bizvisionsoft.pms.projecttemplate.SaveAsOBSModuleACT
+ * <p>
+ * 7.打开变更操作修改
+ * 行操作"打开变更"修改:(操作类型:自定义操作,图标:/img/right.svg,样式:默认,插件标识:com.bizvisionsoft.pms,类名:com.bizvisionsoft.pms.projectchange.OpenProjectChangeACT)
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ *
+ *
+ *
+ */
