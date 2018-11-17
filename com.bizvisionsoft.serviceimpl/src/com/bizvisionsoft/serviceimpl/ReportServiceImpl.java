6e6666b2f6b95c1612f36775002ef3594232881f
hua <ghuazh@gmail.com>
2018-11-17 22:17:53 +0800

    Signed-off-by: hua <ghuazh@gmail.com>

--- com.bizvisionsoft.serviceimpl/src/com/bizvisionsoft/serviceimpl/ReportServiceImpl.java
+++ com.bizvisionsoft.serviceimpl/src/com/bizvisionsoft/serviceimpl/ReportServiceImpl.java
@@ -6,6 +6,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -73,23 +74,14 @@ public class ReportServiceImpl extends BasicServiceImpl implements ReportService
 
 	@Override
 	public Response commandReport(String commandParam) {
-		Document command = Document.parse(commandParam);
-		String outputType = command.getString("outputType");
-		String template = command.getString("template");
-		String fileName = command.getString("fileName");
-		String input_obj_id = command.getString("input_obj_id");
-		String selected_id = command.getString("selected_id");
-		String page_input_id = command.getString("page_input_id");
-		String root_input_id = command.getString("root_input_id");
-		JQ jq = new JQ(command.getString("jq"));
-		if (Check.isAssigned(input_obj_id))
-			jq.set("input_obj_id", new ObjectId(input_obj_id));
-		if (Check.isAssigned(selected_id))
-			jq.set("selected_id", new ObjectId(selected_id));
-		if (Check.isAssigned(page_input_id))
-			jq.set("page_input_id", new ObjectId(page_input_id));
-		if (Check.isAssigned(root_input_id))
-			jq.set("root_input_id", new ObjectId(root_input_id));
+		Document cmd = Document.parse(commandParam);
+		String outputType = cmd.getString("outputType");
+		String template = cmd.getString("template");
+		String fileName = cmd.getString("fileName");
+
+		JQ jq = new JQ(cmd.getString("jq"));
+		Arrays.asList("input_obj_id", "selected_id", "page_input_id", "root_input_id")
+				.forEach(s -> Check.isAssigned(cmd.getString(s), i -> jq.set(s, new ObjectId(i))));
 
 		String filePath = Service.rptDesignFolder.getPath() + "/" + template;
 		try {
