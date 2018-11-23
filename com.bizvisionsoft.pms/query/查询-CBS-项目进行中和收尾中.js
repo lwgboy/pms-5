ee49ddeb31089c1afab33f422abf015906f240bd
gdiyang <gdiyang@lode>
2018-11-23 09:44:33 +0800

    预算成本对比分析表增加领域、承研部门、项目经理进行查询
    Signed-off-by: gdiyang <gdiyang@lode>

--- com.bizvisionsoft.pms/query/查询-CBS-项目进行中和收尾中.js
+++ com.bizvisionsoft.pms/query/查询-CBS-项目进行中和收尾中.js
@@ -1,81 +1,27 @@
-[
-		{
-			"$lookup" : {
-				"from" : "project",
-				"localField" : "_id",
-				"foreignField" : "cbs_id",
-				"as" : "project"
-			}
-		},
-		{
-			"$unwind" : "$project"
-		},
-		{
-			"$match" : {
-				"project.status" : {
-					"$nin" : [ "已创建", "已关闭" ]
-				}
-			}
-		},
-		{
-			"$addFields" : {
-				"scopeId" : "$project.id"
-			}
-		},
-		{
-			"$lookup" : {
-				"from" : "user",
-				"let" : {
-					"userId" : "$project.pmId"
-				},
-				"pipeline" : [ {
-					"$match" : {
-						"$expr" : {
-							"$eq" : [ "$$userId", "$userId" ]
-						}
-					}
-				}, {
-					"$project" : {
-						"_id" : false,
-						"userId" : true,
-						"name" : true,
-						"headPics" : true,
-						"position" : true,
-						"tel" : true,
-						"org_id" : true,
-						"email" : true
-					}
-				} ],
-				"as" : "scopeCharger_meta"
-			}
-		},
-		{
-			"$unwind" : {
-				"path" : "$scopeCharger_meta",
-				"preserveNullAndEmptyArrays" : true
-			}
-		},
-		{
-			"$lookup" : {
-				"from" : "organization",
-				"localField" : "scopeCharger_meta.org_id",
-				"foreignField" : "_id",
-				"as" : "temp_Org"
-			}
-		},
-		{
-			"$addFields" : {
-				"scopeCharger" : {
-					"$cond" : [ "$scopeCharger_meta.name",
-							"$scopeCharger_meta.name", "" ]
-				},
-				"scopeCharger_meta.orgInfo" : {
-					"$arrayElemAt" : [ "$temp_Org.fullName", 0.0 ]
-				}
-			}
-		}, {
-			"$project" : {
-				"temp_Org" : false,
-				"project" : false
-			}
-		} ]
+[ {
+	"$lookup" : {
+		"from" : "project",
+		"localField" : "_id",
+		"foreignField" : "cbs_id",
+		"as" : "project"
+	}
+}, {
+	"$unwind" : "$project"
+}, {
+	"$match" : {
+		"project.status" : {
+			"$nin" : [ "已创建", "已关闭" ]
+		}
+	}
+}, {
+	"$addFields" : {
+		"scopeId" : "$project.id",
+		"eps_id" : "$project.eps_id",
+		"impUnit_id" : "$project.impUnit_id",
+		"pmId" : "$project.pmId",
+	}
+}, {
+	"$project" : {
+		"project" : false
+	}
+} ]
