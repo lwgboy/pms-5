42758d12f34f19ada7142be90c020efff2bca777
hua <ghuazh@gmail.com>
2018-11-28 07:16:44 +0800

    Signed-off-by: hua <ghuazh@gmail.com>

---
+++ com.awesometech.leoco/query/图表-预算成本.js
@@ -0,0 +1,37 @@
+{
+	"title": [{
+			"text": "<title1>",
+			"y": "1%",
+			"x": "25%",
+			"textAlign": "center"
+		},
+		{
+			"text": "<title2>",
+			"x": "75%",
+			"y": "1%",
+			"textAlign": "center"
+		}
+	],
+	"tooltip": {},
+	"grid": {
+		"left": "3%",
+		"right": "4%",
+		"bottom": "6%",
+		"top": "30%",
+		"containLabel": true,
+		"tooltip": {
+			"trigger": "axis"
+		}
+	},
+	"xAxis": {
+		"type": "category",
+		"data": "<xAxisData>"
+	},
+	"legend": {
+		"type": "scroll",
+		"bottom": 20,
+		"data": "<legendData>"
+	},
+	"yAxis": "<yAxis>",
+	"series": "<series>"
+}
