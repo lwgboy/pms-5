bc2a915b5b17c096913ad0114e9239503dcb3354
hua <ghuazh@gmail.com>
2018-11-27 15:33:00 +0800

    Signed-off-by: hua <ghuazh@gmail.com>

--- com.bizvisionsoft.service/src/com/bizvisionsoft/service/CatalogService.java
+++ com.bizvisionsoft.service/src/com/bizvisionsoft/service/CatalogService.java
@@ -19,52 +19,81 @@ import com.bizvisionsoft.service.model.Catalog;
 public interface CatalogService {
 
 	@POST
-	@Path("/res/selector/org/root/{userId}/")
+	@Path("/root/selector/org/{userId}/")
 	@Consumes("application/json; charset=UTF-8")
 	@Produces("application/json; charset=UTF-8")
-	@DataSet({ "组织资源图表/" + DataSet.LIST })
-	public List<Catalog> listResOrgRoot(@PathParam("userId") @MethodParam(MethodParam.CURRENT_USER_ID) String userId);
+	@DataSet({ "组织资源图表/list" })
+	public List<Catalog> listOrgRoot(@PathParam("userId") @MethodParam(MethodParam.CURRENT_USER_ID) String userId);
 
 	@POST
-	@Path("/res/selector/org/structure/")
+	@Path("/root/selector/eps/")
 	@Consumes("application/json; charset=UTF-8")
 	@Produces("application/json; charset=UTF-8")
-	@DataSet({ "组织资源图表/" + DataSet.STRUCTURE_LIST, "EPS资源图表/" + DataSet.STRUCTURE_LIST, "项目资源图表/" + DataSet.STRUCTURE_LIST })
-	public List<Catalog> listResOrgStructure(@MethodParam(MethodParam.OBJECT) Catalog parent);
+	@DataSet({ "EPS资源图表/list", "EPS预算成本图表/list" })
+	public List<Catalog> listEPSRoot();
+	
+	@POST
+	@Path("/res/selector/structure/")
+	@Consumes("application/json; charset=UTF-8")
+	@Produces("application/json; charset=UTF-8")
+	@DataSet({ "组织资源图表/slist", "EPS资源图表/slist", "项目资源图表/slist" })
+	public List<Catalog> listResStructure(@MethodParam(MethodParam.OBJECT) Catalog parent);
 
 	@POST
-	@Path("/res/selector/org/count/")
+	@Path("/res/selector/count/")
 	@Consumes("application/json; charset=UTF-8")
 	@Produces("application/json; charset=UTF-8")
-	@DataSet({ "组织资源图表/" + DataSet.STRUCTURE_COUNT, "EPS资源图表/" + DataSet.STRUCTURE_COUNT, "项目资源图表/" + DataSet.STRUCTURE_COUNT })
-	public long countResOrgStructure(@MethodParam(MethodParam.OBJECT) Catalog parent);
+	@DataSet({ "组织资源图表/scount", "EPS资源图表/scount", "项目资源图表/scount" })
+	public long countResStructure(@MethodParam(MethodParam.OBJECT) Catalog parent);
 
-	@POST
-	@Path("/res/selector/eps/root/")
-	@Consumes("application/json; charset=UTF-8")
-	@Produces("application/json; charset=UTF-8")
-	@DataSet({ "EPS资源图表/" + DataSet.LIST })
-	public List<Catalog> listResEPSRoot();
 
 	@POST
 	@Path("/res/chart/")
 	@Consumes("application/json; charset=UTF-8")
 	@Produces("application/json; charset=UTF-8")
-	@DataSet({ "组织资源图表/" + DataSet.CHART, "EPS资源图表/" + DataSet.CHART, "项目资源图表/" + DataSet.CHART })
+	@DataSet({ "组织资源图表/chart", "EPS资源图表/chart", "项目资源图表/chart" })
 	public Document createResChart(@MethodParam(MethodParam.CONDITION) Document condition);
+	
+	@POST
+	@Path("/budgetNCost/chart/")
+	@Consumes("application/json; charset=UTF-8")
+	@Produces("application/json; charset=UTF-8")
+	@DataSet({ "EPS预算成本图表/chart" })
+	public Document createBudgetNCostChart(@MethodParam(MethodParam.CONDITION) Document condition);
+	
+	@POST
+	@Path("/budgetNCost/selector/structure/")
+	@Consumes("application/json; charset=UTF-8")
+	@Produces("application/json; charset=UTF-8")
+	@DataSet({ "EPS预算成本图表/slist" })
+	public List<Catalog> listBudgetNCostStructure(@MethodParam(MethodParam.OBJECT) Catalog parent);
 
+	@POST
+	@Path("/budgetNCost/selector/count/")
+	@Consumes("application/json; charset=UTF-8")
+	@Produces("application/json; charset=UTF-8")
+	@DataSet({ "EPS预算成本图表/scount" })
+	public long countBudgetNCostStructure(@MethodParam(MethodParam.OBJECT) Catalog parent);
+	
+	@POST
+	@Path("/budgetcost/option/default")
+	@Consumes("application/json; charset=UTF-8")
+	@Produces("application/json; charset=UTF-8")
+	@DataSet({ "EPS预算成本图表/default" })
+	public Document createDefaultBudgetNCostOption();
+	
 	@POST
 	@Path("/res/option/default")
 	@Consumes("application/json; charset=UTF-8")
 	@Produces("application/json; charset=UTF-8")
-	@DataSet({ "组织资源图表/" + DataSet.DEFAULT, "EPS资源图表/" + DataSet.DEFAULT, "项目资源图表/" + DataSet.DEFAULT })
-	public Document createDefaultOption();
+	@DataSet({ "组织资源图表/default", "EPS资源图表/default", "项目资源图表/default" })
+	public Document createDefaultResOption();
 
 	@POST
 	@Path("/res/selector/project/root/{_id}")
 	@Consumes("application/json; charset=UTF-8")
 	@Produces("application/json; charset=UTF-8")
-	@DataSet({ "项目资源图表/" + DataSet.LIST })
+	@DataSet({ "项目资源图表/list" })
 	public List<Catalog> listResProjectRoot(@PathParam("_id") @MethodParam(MethodParam.ROOT_CONTEXT_INPUT_OBJECT_ID) ObjectId _id);
 
 }
