535b42ebef410aebcbb1680803efbff2249faa14
gdiyang <gdiyang@lode>
2018-12-18 17:08:43 +0800

    界面提示（未完成）
    Signed-off-by: gdiyang <gdiyang@lode>

--- com.bizvisionsoft.pms/src/com/bizvisionsoft/pms/work/gantt/action/CheckSchedule.java
+++ com.bizvisionsoft.pms/src/com/bizvisionsoft/pms/work/gantt/action/CheckSchedule.java
@@ -2,13 +2,17 @@ package com.bizvisionsoft.pms.work.gantt.action;
 
 import java.util.List;
 
+import org.eclipse.jface.dialogs.MessageDialog;
+
 import com.bizivisionsoft.widgets.util.Layer;
 import com.bizvisionsoft.annotations.ui.common.Execute;
 import com.bizvisionsoft.annotations.ui.common.Inject;
 import com.bizvisionsoft.annotations.ui.common.MethodParam;
 import com.bizvisionsoft.bruiengine.service.IBruiService;
+import com.bizvisionsoft.bruiengine.util.CommandHandler;
 import com.bizvisionsoft.service.ProjectService;
 import com.bizvisionsoft.service.WorkSpaceService;
+import com.bizvisionsoft.service.model.ICommand;
 import com.bizvisionsoft.service.model.IWBSScope;
 import com.bizvisionsoft.service.model.Project;
 import com.bizvisionsoft.service.model.Result;
@@ -22,7 +26,7 @@ public class CheckSchedule {
 
 	@Execute
 	public void execute(@MethodParam(Execute.ROOT_CONTEXT_INPUT_OBJECT) IWBSScope rootInput) {
-		//TODO 使用CommandHandler进行处理
+		// TODO 使用CommandHandler进行处理
 		if (rootInput != null) {
 			Workspace workspace = rootInput.getWorkspace();
 			if (workspace != null) {
@@ -35,21 +39,35 @@ public class CheckSchedule {
 				}
 				if (project != null && project.getChangeStatus() != null && "变更中".equals(project.getChangeStatus()))
 					checkManageItem = false;
-				
+
 				List<Result> result = Services.get(WorkSpaceService.class).schedulePlanCheck(workspace, checkManageItem);
 
-//				if (Result.CODE_WORK_SUCCESS == result.code) {
-//					Layer.message(result.message);
-//				} else if (Result.CODE_UPDATEMANAGEITEM == result.code) {
-//					brui.error( "检查结果",
-//							"管理节点 <b style='color:red;'>" + result.data.getString("name") + "</b> 的完成时间超过限定。");
-//				} else if (Result.CODE_UPDATESTAGE == result.code) {
-//					brui.error( "检查结果",
-//							"工作 <b style='color:red;'>" + result.data.getString("name") + "</b> 的完成时间超过阶段限定。");
-//				} else if (Result.CODE_UPDATEPROJECT == result.code) {
-//					brui.error("检查结果",
-//							"工作 <b style='color:red;'>" + result.data.getString("name") + "</b> 的完成时间超过項目限定。");
-//				}
+				if (result.stream().anyMatch(r -> Result.TYPE_ERROR == r.type)) {// 错误的提示
+
+					return ;
+				} else if (result.stream().anyMatch(r -> Result.TYPE_QUESTION == r.type)) {// 询问的提示
+
+					return ;
+				}else if (result.stream().anyMatch(r -> Result.TYPE_WARNING == r.type)) {// 警告的提示
+
+					return ;
+				}
+
+				// if (Result.CODE_WORK_SUCCESS == result.code) {
+				// Layer.message(result.message);
+				// } else if (Result.CODE_UPDATEMANAGEITEM == result.code) {
+				// brui.error( "检查结果",
+				// "管理节点 <b style='color:red;'>" + result.data.getString("name") + "</b>
+				// 的完成时间超过限定。");
+				// } else if (Result.CODE_UPDATESTAGE == result.code) {
+				// brui.error( "检查结果",
+				// "工作 <b style='color:red;'>" + result.data.getString("name") + "</b>
+				// 的完成时间超过阶段限定。");
+				// } else if (Result.CODE_UPDATEPROJECT == result.code) {
+				// brui.error("检查结果",
+				// "工作 <b style='color:red;'>" + result.data.getString("name") + "</b>
+				// 的完成时间超过項目限定。");
+				// }
 			}
 		}
 	}
