31d85e96d756f9f6c952bccb722f00271f528da7
hua <ghuazh@gmail.com>
2018-12-24 11:22:22 +0800

    Command 支持 Locale

--- com.bizvisionsoft.service/src/com/bizvisionsoft/service/model/Command.java
+++ com.bizvisionsoft.service/src/com/bizvisionsoft/service/model/Command.java
@@ -1,11 +1,14 @@
 package com.bizvisionsoft.service.model;
 
 import java.util.Date;
+import java.util.Locale;
 
 import org.bson.Document;
 import org.bson.types.ObjectId;
 
-public class Command{
+import com.bizvisionsoft.service.tools.NLS;
+
+public class Command {
 
 	public Date date;
 
@@ -21,7 +24,11 @@ public class Command{
 
 	public String consignerName;
 
-	public static Command newInstance(String name, User user, User consigner, Date date, ObjectId _id) {
+	public String lang;
+
+	public String locale;
+
+	public static Command newInstance(String name, User user, User consigner, Date date, ObjectId _id, Locale locale) {
 		Command c = new Command();
 		c.name = name;
 		c.userId = user.getUserId();
@@ -30,11 +37,13 @@ public class Command{
 		c.consignerName = consigner.getName();
 		c.date = date;
 		c._id = _id;
+		c.lang = locale.getLanguage();
+		c.locale = locale.getCountry();
 		return c;
 	}
 
-	public static Command newInstance(String name, String userId, String userName, String consignerId,
-			String consignerName, Date date, ObjectId _id) {
+	public static Command newInstance(String name, String userId, String userName, String consignerId, String consignerName, Date date,
+			ObjectId _id, Locale locale) {
 		Command c = new Command();
 		c.name = name;
 		c.userId = userId;
@@ -43,12 +52,18 @@ public class Command{
 		c.consignerName = consignerName;
 		c.date = date;
 		c._id = _id;
+		c.lang = locale.getLanguage();
+		c.locale = locale.getCountry();
 		return c;
 	}
 
 	public Document info() {
-		return new Document("date", date).append("userId", userId).append("userName", userName)
-				.append("consignerId", consignerId).append("consignerName", consignerName);
+		return new Document("date", date).append("userId", userId).append("userName", userName).append("consignerId", consignerId)
+				.append("consignerName", consignerName);
+	}
+
+	public String nls(String text) {
+		return NLS.get(lang, text);
 	}
 
 }
