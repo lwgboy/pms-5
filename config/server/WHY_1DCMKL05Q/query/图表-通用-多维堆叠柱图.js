d7d53ffb4484dee2d96df4780174abf29958d7f0
hua <ghuazh@gmail.com>
2019-06-07 00:04:37 +0800

    Signed-off-by: hua <ghuazh@gmail.com>

---
+++ config/server/WHY_1DCMKL05Q/query/图表-通用-多维堆叠柱图.js
@@ -0,0 +1,38 @@
+{
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+            type: 'shadow'
+        }
+    },
+    legend: {
+        data: "<legend>"
+    },
+    toolbox: {
+        show: true,
+        orient: 'vertical',
+        left: 'right',
+        top: 'center',
+        feature: {
+            mark: {show: true},
+            dataView: {show: true, readOnly: false},
+            magicType: {show: true, type: ['line', 'bar', 'stack', 'tiled']},
+            restore: {show: true},
+            saveAsImage: {show: true}
+        }
+    },
+    calculable: true,
+    xAxis: [
+        {
+            type: 'category',
+            axisTick: {show: false},
+            data: "<xAxis>"
+        }
+    ],
+    yAxis: [
+        {
+            type: 'value'
+        }
+    ],
+    series:"<series>"
+}
