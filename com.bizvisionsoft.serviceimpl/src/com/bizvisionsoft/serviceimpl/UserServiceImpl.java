9984dcf090b5c368561664eada24ddf4d140de77
hua <ghuazh@gmail.com>
2019-06-06 22:33:48 +0800

    Signed-off-by: hua <ghuazh@gmail.com>

--- com.bizvisionsoft.serviceimpl/src/com/bizvisionsoft/serviceimpl/UserServiceImpl.java
+++ com.bizvisionsoft.serviceimpl/src/com/bizvisionsoft/serviceimpl/UserServiceImpl.java
@@ -59,7 +59,7 @@ public class UserServiceImpl extends BasicServiceImpl implements UserService {
 		if (userDoc == null)
 			throw new ServiceException("账户无法通过验证");
 
-//		if (!logger.isDebugEnabled()) {
+		if (!logger.isDebugEnabled()) {
 			try {
 				String _psw = encryPassword(userDoc.getObjectId("_id").toHexString(), password);
 				if (!userDoc.getString("password").equals(_psw)) {
@@ -68,7 +68,7 @@ public class UserServiceImpl extends BasicServiceImpl implements UserService {
 			} catch (Exception e) {
 				throw new ServiceException("账户无法通过验证");
 			}
-//		}
+		}
 
 		String domain = userDoc.getString("domain");
 		if (domain == null)
