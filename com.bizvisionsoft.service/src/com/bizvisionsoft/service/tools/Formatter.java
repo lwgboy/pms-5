a40d6e411e02dfed8d9b39a55ddd72668ccff417
hua <ghuazh@gmail.com>
2019-06-13 01:33:26 +0800

    Signed-off-by: hua <ghuazh@gmail.com>

--- com.bizvisionsoft.service/src/com/bizvisionsoft/service/tools/Formatter.java
+++ com.bizvisionsoft.service/src/com/bizvisionsoft/service/tools/Formatter.java
@@ -761,4 +761,38 @@ public class Formatter {
 		}
 		return decompressed;
 	}
+
+	public static String getFirstString(String... text) {
+		for (int i = 0; i < text.length; i++) {
+			if (text[i] != null && !text[i].isEmpty())
+				return text[i];
+		}
+		return null;
+	}
+
+	public static int getMaxCommonDivisor(int a, int b) {
+		// 定义一个交换站值
+		int temp = 0;
+		while (a % b != 0) {
+			temp = a % b;
+			a = b;
+			b = temp;
+		}
+		return b;
+	}
+
+	// 求两个数的最小公倍数（两个数相乘 等于 这两个数的最大公约数和最小公倍数的 积）
+	public static int getMinCommonMultiple(int a, int b) {
+		return a * b / getMaxCommonDivisor(a, b);
+	}
+
+	// 求多个数的最小公倍数
+	public static int getMinMultiCommonMultiple(int... arrays) {
+		int val = arrays[0];
+		// 实现原理：拿前两个数的最小公约数和后一个数比较，求他们的公约数以此来推。。。
+		for (int i = 1; i < arrays.length; i++) {
+			val = getMinCommonMultiple(val, arrays[i]);
+		}
+		return val;
+	}
 }
