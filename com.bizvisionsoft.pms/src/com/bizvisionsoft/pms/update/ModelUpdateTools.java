a7b4e50976d2b1d088259f20d56baf01e5c0db92
hua <ghuazh@gmail.com>
2019-06-20 11:36:00 +0800

    Signed-off-by: hua <ghuazh@gmail.com>

--- com.bizvisionsoft.pms/src/com/bizvisionsoft/pms/update/ModelUpdateTools.java
+++ com.bizvisionsoft.pms/src/com/bizvisionsoft/pms/update/ModelUpdateTools.java
@@ -1,4 +1,4 @@
-package com.bizvisionsoft.bruicommons;
+package com.bizvisionsoft.pms.update;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -14,6 +14,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Optional;
 
+import com.bizvisionsoft.bruicommons.ModelLoader;
 import com.bizvisionsoft.bruicommons.model.Assembly;
 import com.bizvisionsoft.bruicommons.model.Site;
 import com.google.gson.GsonBuilder;
