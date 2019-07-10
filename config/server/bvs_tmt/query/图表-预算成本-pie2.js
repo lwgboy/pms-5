948b54d371465144671465c733cd98ac617c2430
zhangjuncai <ThinkPad@pc>
2019-07-10 11:12:42 +0800

    移除部分历史项目

--- config/server/bvs_tmt/query/图表-预算成本-pie2.js
+++
@@ -1,8 +0,0 @@
-{
-    "name": "<name>",
-    "type": "pie",
-    "center": [ "75%", "20%" ],
-    "radius": "30%",
-    "z": 100,
-    "data": "<data>"
-}
--- config/server/leo_1DEQR41EQ/query/topsreport/pipeline-d1cft.js
+++
@@ -1,9 +0,0 @@
-[ {
-	"$match" : {
-		"problem_id" :"<id>"
-	}
-}, {
-	"$sort" : {
-		"role" : 1
-	}
-} ]
