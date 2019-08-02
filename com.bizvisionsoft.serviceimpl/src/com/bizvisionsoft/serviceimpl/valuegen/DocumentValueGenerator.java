c5fb477baf5aa33191cde9b4da82fc163e94aecd
hua <ghuazh@gmail.com>
2019-08-02 12:49:17 +0800

    Signed-off-by: hua <ghuazh@gmail.com>

--- com.bizvisionsoft.serviceimpl/src/com/bizvisionsoft/serviceimpl/valuegen/DocumentValueGenerator.java
+++ com.bizvisionsoft.serviceimpl/src/com/bizvisionsoft/serviceimpl/valuegen/DocumentValueGenerator.java
@@ -225,7 +225,7 @@ public class DocumentValueGenerator extends SystemServiceImpl implements IValueG
 		return text;
 	}
 
-	public static void generateDocumentValue(ValueRule vr, Document doc) {
+	public static void runGenerate(ValueRule vr, Document doc) {
 		String value = new DocumentValueGenerator(vr).getValue(doc);
 		doc.append(vr.fieldName, value);		
 	}
