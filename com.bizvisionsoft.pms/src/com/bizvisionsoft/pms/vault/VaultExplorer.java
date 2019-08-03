7940d1d8bdd63e7672e4720a864c38253f276c87
hua <ghuazh@gmail.com>
2019-08-03 21:59:03 +0800

    Signed-off-by: hua <ghuazh@gmail.com>

--- com.bizvisionsoft.pms/src/com/bizvisionsoft/pms/vault/VaultExplorer.java
+++ com.bizvisionsoft.pms/src/com/bizvisionsoft/pms/vault/VaultExplorer.java
@@ -2,7 +2,6 @@ package com.bizvisionsoft.pms.vault;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Stream;
 
 import org.eclipse.swt.SWT;
