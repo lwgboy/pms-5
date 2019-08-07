b9f3ab65c8d5a622c6700b57bca5704f52e052f3
hua <ghuazh@gmail.com>
2019-08-07 11:36:29 +0800

    Signed-off-by: hua <ghuazh@gmail.com>

---
+++ com.bizvisionsoft.pms/src/com/bizvisionsoft/pms/vault/FolderDocSelector.java
@@ -0,0 +1,20 @@
+package com.bizvisionsoft.pms.vault;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Shell;
+
+import com.bizvisionsoft.bruiengine.ui.Part;
+
+public class FolderDocSelector extends Part {
+
+	protected FolderDocSelector(Shell parentShell) {
+		super(parentShell);
+	}
+
+	@Override
+	protected void createContents(Composite parent) {
+		
+	}
+	
+
+}
---
+++ com.bizvisionsoft.pms/src/com/bizvisionsoft/pms/vault/SelectFolderOrDocACT.java
@@ -0,0 +1,21 @@
+package com.bizvisionsoft.pms.vault;
+
+import com.bizvisionsoft.annotations.ui.common.Execute;
+import com.bizvisionsoft.annotations.ui.common.Inject;
+import com.bizvisionsoft.bruiengine.service.IBruiContext;
+import com.bizvisionsoft.bruiengine.service.IBruiService;
+
+public class SelectFolderOrDocACT {
+
+
+	@Inject
+	private IBruiService br;
+
+	@Inject
+	private IBruiContext context;
+
+	@Execute
+	public void execute() {
+		
+	}
+}
