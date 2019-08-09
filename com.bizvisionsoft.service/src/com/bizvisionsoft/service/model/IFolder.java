f17688155fb89a91213b9e76c7cdae820b8c5328
hua <ghuazh@gmail.com>
2019-08-09 15:07:50 +0800

    Signed-off-by: hua <ghuazh@gmail.com>

--- com.bizvisionsoft.service/src/com/bizvisionsoft/service/model/IFolder.java
+++ com.bizvisionsoft.service/src/com/bizvisionsoft/service/model/IFolder.java
@@ -7,42 +7,9 @@ import com.bizvisionsoft.service.tools.Formatter;
 public interface IFolder {
 
 	public final static IFolder Null = new VaultFolder().set_id(Formatter.ZeroObjectId());
-
-	/**
-	 * 创建一个只有_id的目录对象
-	 * 
-	 * @param _id
-	 * @return
-	 */
+	
 	public static IFolder newInstance(ObjectId _id) {
-		IFolder fld = new IFolder() {
-
-			@Override
-			public ObjectId get_id() {
-				return _id;
-			}
-
-			@Override
-			public IFolder setName(String name) {
-				return this;
-			}
-
-			@Override
-			public String getName() {
-				return "";
-			}
-
-			@Override
-			public IFolder getContainer() {
-				return null;
-			}
-
-			@Override
-			public boolean isContainer() {
-				return false;
-			}
-		};
-		return fld;
+		return new VaultFolder().set_id(_id);
 	}
 
 	ObjectId get_id();
